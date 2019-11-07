69dd502b58bc4810fc5c765e9b78e5fed6f78d5f
serena Lee <dearserenalee@gmail.com>
2019-11-07 19:14:48 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -18,6 +18,6 @@ def speak_to_grandma(phrase)
     elsif phrase == phrase.upcase
     return "NO, NOT SINCE 1938!"
   else phrase == "I LOVE YOU GRANDMA!"
-    return ""
+    return "I LOVE YOU TOO PUMPKIN!"
   end
 end 
